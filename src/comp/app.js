bfa74f4a764ec3936b69153ab727619a99837e21
Fenena <xitan12@gmail.com>
2021-06-05 17:50:36 +0300

    Дз по React

--- src/comp/app.js
+++ src/comp/app.js
@@ -4,15 +4,13 @@ import "../styles/app.css";
 import Header from "./Header";
 import Footer from "./Footer";
 
-class app extends Component {
-	render(){
-		return (
-			<Fragment>
-				<Header />
-				<Footer />
-			</Fragment>
-		);
-	}
+function app(){
+	return (
+		<Fragment>
+			<Header />
+			<Footer />
+		</Fragment>
+	);	
 }
 
 export default app;
